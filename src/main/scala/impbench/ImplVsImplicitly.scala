b442424bc8c710cc524f5fe68963617b820d2953
Lloyd <lloydmeta@gmail.com>
2016-04-17 21:05:16 +0900

    Slight mods
    [info] Benchmark                  Mode  Cnt  Score   Error  Units
    [info] ImplVsImplicitly.baseline  avgt   30  3.268 ± 0.025  ns/op
    [info] ImplVsImplicitly.explicit  avgt   30  3.292 ± 0.036  ns/op
    [info] ImplVsImplicitly.imply     avgt   30  3.291 ± 0.039  ns/op

---
+++ src/main/scala/impbench/ImplVsImplicitly.scala
@@ -0,0 +1,28 @@
+package impbench
+
+import java.util.concurrent.TimeUnit
+
+import imp.imp
+import org.openjdk.jmh.annotations._
+
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+class ImplVsImplicitly {
+
+  def addTogetherBase[A: Addable](x: A, y: A) = implicitly[Addable[A]].add(x, y)
+  def addTogetherImp[A: Addable](x: A, y: A) = imp[Addable[A]].add(x, y)
+  def addTogetherExplicit[A](x: A, y: A)(implicit addable: Addable[A]) = addable.add(x, y)
+
+  @Benchmark
+  def baseline = addTogetherBase(1, 1)
+
+  @Benchmark
+  def imply = addTogetherImp(1, 1)
+
+  @Benchmark
+  def explicit = addTogetherExplicit(1, 1)
+
+
+}
